b9634e58ac8149df5adda34cff0070ce5ed1e71f
Blair Crumbly <blaircrumbly@gmail.com>
2024-12-27 19:02:41 -0500

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+
+function countDown(start) {
+    while (start >= 0) {
+      console.log(start);
+      start--;
+    }
+}
+
+
